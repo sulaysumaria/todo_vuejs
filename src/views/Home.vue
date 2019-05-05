a5fd01925a542f11a9b293e97cc00657ba1bd272
Sulay Sumaria <sulaysumaria@gmail.com>
2019-05-05 19:26:12 +0530

    Initial Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Todos from "./../components/Todos";
+import AddTodo from "./../components/AddTodo";
+
+export default {
+  name: "app",
+  components: {
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: []
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(() => {
+          this.todos = this.todos.filter(a => a.id !== id);
+        })
+        .catch(e => console.log(e));
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed
+        })
+        .then(res => {
+          this.todos.push(res.data);
+        })
+        .catch(e => console.log(e));
+    }
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then(res => {
+        this.todos = res.data;
+      })
+      .catch(e => console.log(e));
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
+}
+</style>
